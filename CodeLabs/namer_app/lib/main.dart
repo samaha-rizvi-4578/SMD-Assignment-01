fd049bcfb9b003f4bb07205e832e423d76a8ccd3
SamahaRizvi <k214578@nu.edu.pk>
2025-02-12 21:53:13 +0500

    add like button

--- CodeLabs/namer_app/lib/main.dart
+++ CodeLabs/namer_app/lib/main.dart
@@ -32,6 +32,17 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+  
+  void toggleFavorite(){
+    if(favorites.contains(current)){
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -39,6 +50,13 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
+
+  IconData icon;
+  if(appState.favorites.contains(pair)){
+    icon = Icons.favorite;  
+  } else {
+    icon = Icons.favorite_border;
+  }
 
     return Scaffold(                             // ← 3
       body: Center(
@@ -48,11 +66,24 @@ class MyHomePage extends StatelessWidget {
               // ← 5
             BigCard(pair: pair),
             SizedBox(height: 10,),    // ← 6
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
             ),
           ],
         ),
